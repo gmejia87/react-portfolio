d1304216572ebc6f547a10fff4d9576619d99d5b
G Codes <graciela.mejia1@yahoo.com>
2022-11-20 15:33:22 -0800

    add photo to about me

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,11 +1,30 @@
 import React from "react";
+import "./about.css";
+import gmejia from "../../assets/photos/gmejia.jpg";
 
 function About() {
   return (
-    <div>
-      <h2>About me</h2>
-      <p>This section is about me.</p>
-    </div>
+    <section>
+      <div>
+        <h2>About me</h2>
+      </div>
+      <br />
+      <div>
+        <img src={gmejia} alt="Graciela" className="my-photo" />
+      </div>
+      <div>
+        <p>
+          Holla! My name is Graciela Mejia, pronounced 'Gracie-Ella Meh-he-a'
+          but you can call me Gracie if you'd like.
+        </p>
+        <br />
+        <p>
+          Welcome to my portfolio. Please take a look around. On this site
+          you'll find projects I've worked on, my contact information and more.
+          Please feel free to reach out. Gracias!
+        </p>
+      </div>
+    </section>
   );
 }
 
