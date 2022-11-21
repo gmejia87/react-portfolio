3f9ef60476c41c146de6c09237ded7044d4a5b28
G Codes <graciela.mejia1@yahoo.com>
2022-11-20 17:31:33 -0800

    add projects

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,12 +1,26 @@
 import React from "react";
 import "./projects.css";
+import musicenvivo from "../../assets/photos/musicenvivo.png";
+import smarthomie from "../../assets/photos/smarthomie.png";
 
 function Projects() {
   return (
-    <div>
+    <section>
       <h2>My Projects</h2>
+      <br />
       <p>Behold these are my projects!</p>
-    </div>
+
+      <div className="row">
+        <div class="column">
+          <a href="https://gmejia87.github.io/Music-en-Vivo/">
+            <img src={musicenvivo} />
+          </a>
+          <a href="https://smart-homie.herokuapp.com/">
+            <img src={smarthomie} />
+          </a>
+        </div>
+      </div>
+    </section>
   );
 }
 
