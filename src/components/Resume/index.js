d7c5e9a826240856b0bd9a9e76b07915779abc0a
G Codes <graciela.mejia1@yahoo.com>
2022-11-20 18:23:08 -0800

    update resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,10 +1,29 @@
 import React from "react";
+import "./resume.css";
+import { HiDocument } from "react-icons/hi";
 
 function Resume() {
   return (
     <section>
       <h2>My Resume</h2>
-      <p>Here you will find the contents of my resume.</p>
+      <br />
+      <p>
+        <HiDocument />
+        Download my resume.
+      </p>
+      <br />
+
+      <br />
+      <h3>Front-End:</h3>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JavaScript</li>
+      <br />
+      <h3>Back-End:</h3>
+      <li>Node</li>
+      <li>Express</li>
+      <li>React</li>
+      <li>Mongoose</li>
     </section>
   );
 }
