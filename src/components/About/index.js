6c3a1e1bae74773f608e3b35ef1e4c81c65f4d65
G Codes <graciela.mejia1@yahoo.com>
2022-11-20 16:21:15 -0800

    edit about me

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,7 +4,7 @@ import gmejia from "../../assets/photos/gmejia.jpg";
 
 function About() {
   return (
-    <section>
+    <body>
       <div>
         <h2>About me</h2>
       </div>
@@ -13,18 +13,22 @@ function About() {
         <img src={gmejia} alt="Graciela" className="my-photo" />
       </div>
       <div>
-        <p>
+        <p className="intro">
           Holla! My name is Graciela Mejia, pronounced 'Gracie-Ella Meh-he-a'
           but you can call me Gracie if you'd like.
-        </p>
-        <br />
-        <p>
-          Welcome to my portfolio. Please take a look around. On this site
-          you'll find projects I've worked on, my contact information and more.
-          Please feel free to reach out. Gracias!
+          <br />
+          Welcome to my portfolio.
+          <br />
+          Please take a look around. Here you'll find projects I've worked on,
+          my contact information and more. NOTE: I am currently enrolled to UC
+          Davis' Fullstack Flex Web Development bootcamp. I have always been
+          techy and encourage to seek a career in tech, so here I am pursuing
+          that. I look forward to completing this bootcamp and finally moving in
+          a diection that better aligns with my skills and new knowledge. Please
+          feel free to reach out. Gracias!
         </p>
       </div>
-    </section>
+    </body>
   );
 }
 
