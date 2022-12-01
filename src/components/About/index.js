07f9d39c743354faa88d4b66f653fa5d69423b80
G Codes <graciela.mejia1@yahoo.com>
2022-11-30 20:08:02 -0800

    update to components/about

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -14,18 +14,38 @@ function About() {
       </div>
       <div>
         <p className="intro">
-          Holla! My name is Graciela Mejia, pronounced 'Gracie-Ella Meh-he-a'
-          but you can call me Gracie if you'd like.
+          Hola! Welcome to my portfolio.
           <br />
-          Welcome to my portfolio.
           <br />
-          Please take a look around. Here you'll find projects I've worked on,
-          my contact information and more. NOTE: I am currently enrolled to UC
-          Davis' Fullstack Flex Web Development bootcamp. I have always been
-          techy and encourage to seek a career in tech, so here I am pursuing
-          that. I look forward to completing this bootcamp and finally moving in
-          a diection that better aligns with my skills and new knowledge. Please
-          feel free to reach out. Gracias!
+          My name is Graciela Mejia, pronounced 'Gracie-Ella' 'Meh-he-a' but you
+          can call me Gracie if you'd like.
+          <br />
+          <br />
+          Here you'll find projects I've worked on, my contact information and
+          more.
+          <br />
+          <br />
+          I'm currently in the health industry, I have years of customer service
+          experience along with patient care experience, etc.
+          <br />
+          I am currently enrolled to UC Davis' Fullstack Flex Web Development
+          bootcamp.
+          <br />
+          You may be wondering what brought me to this change.
+          <br />
+          Well I have always been good with tech and for years have been highly
+          encouraged to seek a career in tech.
+          <br />
+          Since I was alwasy the go-to person when it came to any tech.
+          <br />
+          I remember doing some light coding in the myspace days, so here I am
+          coding like I had then only better.
+          <br />
+          I am extremely excited to be completing this bootcamp soon, so that I
+          can start applying my skills and new knowledge.
+          <br />
+          <br />
+          ❤️ Graciela
         </p>
       </div>
     </body>
